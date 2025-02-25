a328d355ce9b09a63a139951946659ba779ff032
kecheprathamesh <prathameshkeche007@gmail.com>
2025-02-25 14:09:43 +0530

    updated

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -22,4 +22,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInite83320f61947f489f97cf0d988bc1454::getLoader();
+return ComposerAutoloaderInite83320f61947f489f97cf0d988bc1454::getLoader(); 
